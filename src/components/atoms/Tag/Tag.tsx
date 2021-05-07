8ce28bc1303f8c7cf804fb103190129a22b8e11c
SarthakSri98 <sarthak.srivastava@groww.in>
2021-05-07 19:37:04 +0530

    tag component

---
+++ src/components/atoms/Tag/Tag.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import './tag.css';
+
+const Tag = (props: Props) => {
+
+  if (!React.Children.count(props.children)) {
+    return null
+  }
+
+
+  let tagClass = props.tagClass;
+
+  if (props.isWarning) {
+    tagClass = `tg11Warning`;
+
+  } else if (props.isError) {
+    tagClass = `tg11Error`;
+
+  } else if (props.isInfo) {
+    tagClass = `tg11Info`;
+  }
+
+
+  return (
+    <div className={`tg11Container ${tagClass}`}>
+      {props.children}
+    </div>
+  )
+}
+
+type Props = {
+  isWarning: boolean,
+  isError: boolean,
+  isInfo: boolean,
+  tagClass: string,
+  children: React.ReactNode
+}
+
+Tag.defaultProps = {
+  isWarning: true,
+  isError: false,
+  isInfo: false,
+  tagClass: ''
+} as Props;
+
+export default Tag;
