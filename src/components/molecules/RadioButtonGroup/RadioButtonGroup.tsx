45a6863f0dbff283e4cc518ab5d499fefdf2e51d
Rishav Thakur <rishav.21m@gmail.com>
2021-07-01 18:42:31 +0530

    fix: onSelect type string

--- src/components/molecules/RadioButtonGroup/RadioButtonGroup.tsx
+++ src/components/molecules/RadioButtonGroup/RadioButtonGroup.tsx
@@ -49,7 +49,7 @@ type DefaultProps = {
 type RequiredProps = {
   radioButtons: RadioButtonType[];
   selected: string | number;
-  onSelect: (value?: string | number) => void;
+  onSelect: (value: string) => void;
 }
 
 
