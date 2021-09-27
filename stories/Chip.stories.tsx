deb730c3da21e8cd5b73d61b588c58ccdf77891f
Ankit Chouhan <ankitchouhan1020@gmail.com>
2021-09-27 14:43:34 +0530

    fix: chip margin issue

--- stories/Chip.stories.tsx
+++ stories/Chip.stories.tsx
@@ -3,13 +3,11 @@ import React from 'react';
 import Cancel from '@groww-tech/icon-store/mi/Cancel';
 import FnoIcon from '@groww-tech/icon-store/custom/FnoIcon';
 import { ReactIconProps } from '@groww-tech/icon-store/types';
+
 import { action } from '@storybook/addon-actions';
 import { Story } from '@storybook/react';
 
-import {
-  Chip,
-  MI_ICON_LIST
-} from '../src/components/atoms';
+import { Chip } from '../src/components/atoms';
 import { Props as ChipProps } from '../src/components/atoms/Chip/Chip';
 
 import './style.css';
@@ -18,16 +16,10 @@ export default {
   title: 'Chip',
   component: Chip,
   argTypes: {
-    iconName: {
-      control: {
-        type: 'select',
-        options: Object.keys(MI_ICON_LIST)
-      }
-    },
     iconPosition: {
       control: {
         type: 'select',
-        options: ['left', 'right']
+        options: [ 'left', 'right' ]
       }
     }
   }
@@ -41,7 +33,6 @@ const Template: Story<ChipProps> = (args) => (
 export const Text = Template.bind({});
 Text.args = {
   text: 'NFO',
-  parentClass: 'fs16',
   onClick: action('onChipClick')
 };
 
