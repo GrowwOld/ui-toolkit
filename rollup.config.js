398da1961730b202a084e2bbc8cae8636190894d
Ankit Chouhan <ankitchouhan1020@gmail.com>
2021-11-23 11:13:16 +0530

    initial exp

--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,75 @@
-import babel from 'rollup-plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import external from 'rollup-plugin-peer-deps-external';
-import { terser } from 'rollup-plugin-terser';
-import postcss from 'rollup-plugin-postcss';
-import postcssImport from 'postcss-import';
-import postcssUrl from 'postcss-url';
-import typescript from 'rollup-plugin-typescript';
+import postcssImport from "postcss-import";
+import postcssUrl from "postcss-url";
+import babel from "rollup-plugin-babel";
+import copy from "rollup-plugin-copy";
 import filesize from "rollup-plugin-filesize";
-import copy from 'rollup-plugin-copy'
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
+import typescript from "rollup-plugin-typescript";
 
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+
+import FILE_INPUTS from "./rollup.input";
+
+// import pkg from './package.json';
+
+const commonConfig = {
+  plugins: [
+    commonjs(),
+    postcss({
+      plugins: [ postcssImport(), postcssUrl({ url: 'inline' }) ],
+      extract: true,
+      extensions: [ '.css' ]
+    }),
+    typescript(),
+    babel({
+      exclude: 'node_modules/**',
+      presets: [ '@babel/preset-typescript', '@babel/preset-react' ],
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-private-methods'
+      ]
+    }),
+    external(),
+    resolve(),
+    terser(),
+    filesize(),
+    copy({
+      targets: [
+        { src: 'src/types', dest: 'dist' }
+      ]
+    })
+  ],
+  external: [ 'react', 'react-dom', 'classnames', 'react-lazyload' ]
+
+};
 
 export default [
   {
     input: './src/index.ts',
     output: [
       {
-        file: 'dist/index.js',
-        format: 'cjs'
-      },
-      {
-        file: 'dist/index.es.js',
+        dir: 'dist/es',
         format: 'es',
         exports: 'named'
+      },
+      {
+        dir: 'dist/cjs',
+        format: 'cjs'
       }
     ],
-    plugins: [
-      commonjs(),
-      postcss({
-        plugins: [postcssImport(), postcssUrl({ url: 'inline' })],
-        extract: true,
-        extensions: [".css"],
-      }),
-      typescript(),
-      babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/preset-typescript', '@babel/preset-react'],
-        plugins: [
-          '@babel/plugin-proposal-class-properties',
-          '@babel/plugin-proposal-private-methods',
-        ]
-      }),
-      external(),
-      resolve(),
-      terser(),
-      filesize(),
-      copy({
-        targets: [
-          { src: 'src/types', dest: 'dist' }
-        ]
-      })
+    ...commonConfig
+  },
+  {
+    input: FILE_INPUTS,
+    output: [
+      {
+        dir: 'dist/esm',
+        format: 'cjs'
+      }
     ],
-    external: ['react', 'react-dom', 'classnames', 'react-lazyload']
+    ...commonConfig
   }
 ];
---
+++ rollup.input.js
@@ -0,0 +1,32 @@
+const INPUT_FILES = [
+  './src/components/atoms/Accordion/index.ts',
+  './src/components/atoms/AnimateHeight/index.js',
+  './src/components/atoms/Button/index.ts',
+  './src/components/atoms/CheckBox/index.ts',
+  './src/components/atoms/Chip/index.ts',
+  './src/components/atoms/DateSelector/index.ts',
+  './src/components/atoms/DropDown/index.ts',
+  './src/components/atoms/GoBack/index.ts',
+  './src/components/atoms/Image/index.ts',
+  './src/components/atoms/InformationBoard/index.ts',
+  './src/components/atoms/LaserBeam/index.ts',
+  './src/components/atoms/Loader/index.ts',
+  './src/components/atoms/Popover/index.ts',
+  './src/components/atoms/Popup/index.ts',
+  './src/components/atoms/ProgressBar/index.ts',
+  './src/components/atoms/RadioButton/index.ts',
+  './src/components/atoms/ScrollTop/index.ts',
+  './src/components/atoms/Select/index.ts',
+  './src/components/atoms/Slider/index.ts',
+  './src/components/atoms/Table/index.ts',
+  './src/components/atoms/Tabs/index.ts',
+  './src/components/atoms/Tag/index.ts',
+  './src/components/atoms/TextArea/index.ts',
+  './src/components/atoms/Ticker/index.ts',
+  './src/components/atoms/ToggleSwitch/index.ts',
+  // './src/components/molecules/ToggleSwitch/index.ts',
+  './src/components/molecules/CheckBoxGroup/index.ts',
+  './src/components/molecules/RadioButtonGroup/index.ts'
+];
+
+export default INPUT_FILES;
